a112688452848565fe1ef73b143808ce035fbc89
thebugfoundme <r.deperalta@ttdconsulting.net>
2024-06-14 15:36:13 +0800

    feat: add partner

---
+++ src/API.Tests/Partners/iWyze/IWyzeFactoryTests.cs
@@ -0,0 +1,66 @@
+﻿using API.Interfaces;
+using API.Partners.iWyze;
+using FluentAssertions;
+
+namespace API.Tests.Partners.iWyze
+{
+    internal class IWyzeFactoryTests
+    {
+        private IInsuranceFactory CreateFactory()
+        {
+            return new IWyzeInsuranceFactory();
+        }
+
+        [Test]
+        public void ShouldCreateIWyzeAutoInsurance()
+        {
+            // arrange            
+            IInsuranceFactory factory = CreateFactory();
+
+            // act
+            var actual = factory.CreateAutoInsurance();
+
+            // assert            
+            actual.Should().BeOfType<IWyzeAutoInsurance>();
+        }
+
+        [Test]
+        public void ShouldCreateIWyzeHomeInsurance()
+        {
+            // arrange            
+            IInsuranceFactory factory = CreateFactory();
+
+            // act
+            var actual = factory.CreateHomeInsurance();
+
+            // assert            
+            actual.Should().BeOfType<IWyzeHomeInsurance>();
+        }
+
+        [Test]
+        public void ShouldCreateIWyzeLifeInsurance()
+        {
+            // arrange            
+            IInsuranceFactory factory = CreateFactory();
+
+            // act
+            var actual = factory.CreateLifeInsurance();
+
+            // assert            
+            actual.Should().BeOfType<IWyzeLifeInsurance>();
+        }
+
+        [Test]
+        public void ShouldCreateIWyzeTravelInsurance()
+        {
+            // arrange            
+            IInsuranceFactory factory = CreateFactory();
+
+            // act
+            var actual = factory.CreateTravelInsurance();
+
+            // assert            
+            actual.Should().BeOfType<IWyzeTravelInsurance>();
+        }
+    }
+}
